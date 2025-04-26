60ec7e04a1ed03e9ca6d1bc9e199017ffb30d4a6
DuyKhoiCoder30062004 <136677176+DuyKhoiCoder30062004@users.noreply.github.com>
2025-04-26 22:18:41 +0700

    Update footer.php

---
+++ layout/footer.php
@@ -0,0 +1,36 @@
+<!-- Footer -->
+    <footer class="footer">
+        <div class="footer-section">
+            <h3>ABOUT ZAPPOS</h3>
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Zappos for Good</a></li>
+                <li><a href="#">Zappos at Work</a></li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h3>CUSTOMER SERVICE</h3>
+            <ul>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Contact Info</a></li>
+                <li><a href="#">Shipping And Returns Policy</a></li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h3>RESOURCES</h3>
+            <ul>
+                <li><a href="#">Measurement Guide</a></li>
+                <li><a href="#">Size Conversion Chart</a></li>
+                <li><a href="#">Site Map</a></li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h3>EXPLORE ZAPPOS</h3>
+            <ul>
+                <li><a href="#">Brands</a></li>
+                <li><a href="#">Clothing</a></li>
+                <li><a href="#">Shoes</a></li>
+            </ul>
+        </div>
+        <p class="footer-copy">© 2009–2025 - Zappos.com LLC or its affiliates</p>
+    </footer>
